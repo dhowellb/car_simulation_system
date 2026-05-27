e07cfa348cdf6b1d060b0b1ab624b16d5798cbd9
Duke Buerano <dukebueranoo@gmail.com>
2026-05-27 10:00:00 +0800

    chapter 2: nilagyan ko na ng silinyador yung kotse, ready na bumilis by 5 kada apak

--- car_controller.py
+++ car_controller.py
@@ -4,3 +4,7 @@ class Car:
         self.__year_model = car_year_model
         self.__make = car_make
         self.__speed = 0
+
+        # Part 2: Kailangan nating umarangkada! Ito yung accelerate method natin. Kada tapak sa silinyador, dagdag 5 sa speed natin. Vroom vroom!
+    def accelerate(self):
+        self.__speed += 5
